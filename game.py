890ff3a9786e9c33d08af84246e97af1660c20da
Satyabrata Karan <32532501+satyakaibm@users.noreply.github.com>
2023-09-01 02:57:02 +0200

    Update the input statement

--- game.py
+++ game.py
@@ -4,9 +4,12 @@ print("Welcome to the Monster Slayer game!")
 print("Type 'attack orc' or 'attack dragon' to fight.")
 
 while hero.alive and (orc.alive or dragon.alive):
+    #update the input statement
     command = input("Type: ").lower()
+    #update the command statement
     if command == "attack orc" and orc.alive:
         hero.attack(orc)
+    #update the command statement
     elif command == "attack dragon" and dragon.alive:
         hero.attack(dragon)
     else:
